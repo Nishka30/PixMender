22e89201a5d5a1b8128f1b6db694f8b25dffa4be
Nishka <shrimalinishka@gmail.com>
2024-01-18 16:21:21 +0530

    UI  Updated

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // Initialize Firebase
 var firebaseConfig = {
-	apiKey: "AIzaSyA6VQ3gKwl0sV9Bs3SzrjQ_O0Soow4hPdY",
+  apiKey: "AIzaSyA6VQ3gKwl0sV9Bs3SzrjQ_O0Soow4hPdY",
   authDomain: "imageupload-51b05.firebaseapp.com",
   projectId: "imageupload-51b05",
   storageBucket: "imageupload-51b05.appspot.com",
@@ -14,31 +14,30 @@ var storage = firebase.storage();
 
 var imagesRef = storage.ref("images");
 
-document.getElementById("imageInput").addEventListener("change", function(event) {
-	var file = event.target.files[0];
+document.getElementById("imageInput").addEventListener("change", function (event) {
+  var file = event.target.files[0];
 
-	var imageRef = imagesRef.child(file.name);
+  var imageRef = imagesRef.child(file.name);
 
-	uploadImage(imageRef, file);
+  uploadImage(imageRef, file);
 });
 
 function uploadImage(imageRef, file) {
-    var progressBar = document.getElementById("progressBar");
-  
-    imageRef.put(file).then(function(snapshot) {
-      snapshot.ref.getDownloadURL().then(function(downloadURL) {
-        console.log("Image uploaded successfully: " + downloadURL);
-  
-        // Display an alert after successfully uploading the picture
-        alert("Image uploaded successfully: " + downloadURL);
-      });
-    }).catch(function(error) {
-      console.error("Error uploading image: " + error);
-      alert("Error uploading image: " + error); // Display an alert in case of an error
-    }).on("state_changed", function(snapshot) {
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-  
-      progressBar.value = progress;
-    });
-  }
-  
+  var progressBar = document.getElementById("progressBar");
+
+  imageRef.put(file).then(function (snapshot) {
+    snapshot.ref.getDownloadURL().then(function (downloadURL) {
+      console.log("Image uploaded successfully: " + downloadURL);
+
+            // Display an alert after successfully uploading the picture
+            alert("Image uploaded successfully: " + downloadURL);
+          });
+        }).catch(function (error) {
+          console.error("Error uploading image: " + error);
+          alert("Error uploading image: " + error); // Display an alert in case of an error
+        }).on("state_changed", function (snapshot) {
+          var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+          progressBar.value = progress;
+        });
+      }
